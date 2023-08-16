d8c011fc0e0f174063f34efc32aed06b8ae26427
gathumartins <gathumartins@gmail.com>
2023-08-16 11:05:43 +0300

    added changed tailwing config file and globals.css file in the folder, added the assets inside public folder and create the page and layout files inside the app folder

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'Promptopia',
+    description: 'Discover & Share AI Prompts'
+}
+
+const layout = () => {
+  return (
+   <html lang="en">
+<body>
+    <div className="main">
+        <div className="gradient"/>
+    </div>
+    <main className="app">
+        {children}
+    </main>
+</body>
+   </html>
+  )
+}
+
+export default layout
